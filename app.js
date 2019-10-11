598253ca52d2c7e568df6d0457299392d0901ea3
NilsLandelius <nilsoskar.landelius@gmail.com>
2019-10-11 15:50:27 +0200

    created package.json and app.yaml for GCP.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 require('./startup/routes')(app);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port,()=> console.log('Listening on port ${port}'));
 
 module.exports= server;
